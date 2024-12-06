d8acfa94c26475949d913bb40145ed3c73fd97fe
domsauter <159060903+domsauter@users.noreply.github.com>
2024-12-06 15:42:58 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 from typing import Optional
+import math
 
 class Calculator:
     DICHTE_STAHL = 7.87 * 10**(-6)
-    PI = 3.14159
 
     @staticmethod
     def stueckpreis_berechnen(werkstoff: str, durchschnitt: float, durchmesser: int, laenge: int) -> Optional[str]:
@@ -10,7 +10,7 @@ class Calculator:
             return None
 
         radius = durchmesser / 2
-        volumen_stahl = Calculator.PI * radius**2 * laenge
+        volumen_stahl = math.pi * radius**2 * laenge
         masse_stahl = Calculator.DICHTE_STAHL * volumen_stahl
         stueckpreis = masse_stahl * durchschnitt
         return f"Der aktuelle Materialpreis für den Werkstoff {werkstoff} in der Abmessung D{durchmesser}x{laenge} beträgt: {stueckpreis:.2f} €/Stk."
